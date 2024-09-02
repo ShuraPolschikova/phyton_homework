fbf3c7efbbbd9ec8eb0a38da8862065782136891
Shura <shura_agent_007@mail.ru>
2024-09-02 13:49:30 +0500

    02.09 changes

--- module_3/module_3_4.py
+++ module_3/module_3_4.py
@@ -1,9 +1,15 @@
 def single_root_words(root_word, *other_words):
     same_words = list()
+    words = []
+
     for i in range(len(other_words)):
-        if root_word in other_words[i] or other_words[i] in root_word:
+        root_word = root_word.lower()
+        words = str(other_words[i])
+
+        if root_word in words.lower() or words.lower() in root_word:
             same_words.append(other_words[i])
+
     print(same_words)
 
-single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+single_root_words('rich', 'richest', 'orichalcum', 'cheers', 'richies')
 single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
