2874bb7f1f2fb6df5485d2c392d4e2b42d34e8d3
Shura <shura_agent_007@mail.ru>
2024-09-02 23:58:55 +0500

    02.09 changes

---
+++ module_4/true_math.py
@@ -0,0 +1,10 @@
+def divide(first, second):
+    if second == 0:
+        from math import inf
+        result = inf
+    else:
+        result = first / second
+    return result
+
+divide (10, 0)
+
