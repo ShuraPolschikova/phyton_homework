cdd57be962b782a7e9bbb7a1d8b5650e84d12434
Shura <shura_agent_007@mail.ru>
2024-09-23 18:58:50 +0500

    23.09 done with changes

--- module_5/module_5_3.py
+++ module_5/module_5_3.py
@@ -2,14 +2,13 @@ class House:
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
+
     def __len__(self):
          return self.number_of_floors
 
-    def __len__(self):
-        return self.number_of_floors
-
     def __str__(self):
         return (f'Название: {self.name}, кол-во этажей {self.number_of_floors}')
+
     def __eq__(self, other):
         if isinstance(other.number_of_floors, int) and isinstance(other, House):
             return self.number_of_floors == other.number_of_floors
