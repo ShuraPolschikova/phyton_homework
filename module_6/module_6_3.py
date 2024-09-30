d80acea4a3c3af041ea30f9c6671db8b6d02f2bc
Shura <shura_agent_007@mail.ru>
2024-10-01 00:30:31 +0500

    30.09 done

---
+++ module_6/module_6_3.py
@@ -0,0 +1,39 @@
+class Horse:
+    x_distance = 0
+    sound1 = 'Frrr'
+
+    def run(self, dx):
+        self.x_distance = self.x_distance+dx
+
+class Eagle:
+    y_distance = 0
+    sound2 = 'I train, eat, sleep, and repeat'
+    def fly(self, dy):
+        self.y_distance = self.y_distance+dy
+
+class Pegasus(Horse, Eagle):
+
+    def __init__(self):
+        self.sound = super().sound1
+        self.sound = super().sound2
+
+    def move(self, dx, dy):
+        self.run(dx)
+        self.fly(dy)
+
+    def get_pos(self):
+        pos = (self.x_distance, self.y_distance)
+        return pos
+
+    def voice(self):
+        print(self.sound)
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
