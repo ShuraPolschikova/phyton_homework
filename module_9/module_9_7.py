0ccab74c167a5a2e9d3f3dbc1f5445c900676a57
Shura <shura_agent_007@mail.ru>
2024-10-24 16:51:54 +0500

    24.10 done

---
+++ module_9/module_9_7.py
@@ -0,0 +1,30 @@
+def is_prime(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args,**kwargs)
+
+        if result == 0:
+            print("Число 0 не является ни простым, ни составным")
+            return result
+        elif result == 1:
+            print("Число 1 не является ни простым, ни составным")
+            return result
+        elif result >= 2:
+            for i in range(2, result):
+                if result % i == 0:
+                    print("Составное")
+                    return result
+                else:
+                    print("Простое")
+                    return result
+        return result
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    return a+b+c
+
+
+result = sum_three(1, 1, 1)
+print(result)
+
