718209fb8f6c78bc2e021119c894130f5e8e449b
Shura <shura_agent_007@mail.ru>
2024-11-21 00:02:26 +0500

    20.11

--- module_12/module_12_1.py
+++ module_12/module_12_1.py
@@ -1,6 +1,7 @@
 import unittest
 import module_12_1_shablon as m12
 
+
 class RunnerTest(unittest.TestCase):
     def test_walk(self):
         runner = m12.Runner("Runner1")
@@ -8,13 +9,11 @@ class RunnerTest(unittest.TestCase):
             runner.walk()
         self.assertEqual(runner.distance, 50)
 
-
     def test_run(self):
         runner = m12.Runner("Runner2")
         for i in range(10):
             runner.run()
         self.assertEqual(runner.distance, 100)
-
 
     def test_challenge(self):
         runner1 = m12.Runner("Runner1")
