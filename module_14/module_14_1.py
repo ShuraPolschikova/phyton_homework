d1c766ecf6a8a60b99b8788f6145271677928c35
Shura <shura_agent_007@mail.ru>
2024-12-17 18:48:54 +0500

    17.12.24

---
+++ module_14/module_14_1.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
+
+for i in range(1, 11):
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?,?,?,?)",
+                   (f"(User{i})", f"(example{i}@gmail.com)", 10*i, 1000))
+
+cursor.execute("UPDATE Users SET balance = BALANCE - 500 WHERE id % 2 = 1")
+cursor.execute("DELETE FROM Users WHERE id % 3 = 1")
+
+cursor.execute("SELECT username, email, age, balance FROM Users WHERE age!=60")
+result = cursor.fetchall()
+
+for user in result:
+    print(f"Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}")
+connection.commit()
+connection.close()
