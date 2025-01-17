843d8c3c8eeffc2f75326907fccdd9d0c6b93197
Shura <shura_agent_007@mail.ru>
2025-01-17 13:15:16 +0500

    17.01.25

---
+++ module_16/module_16_1.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def welcome():
+    return "Главная страница"
+
+
+@app.get("/users/Admin")
+async def welcome_admin():
+    return "Вы вошли как администратор"
+
+
+@app.get("/users/{user_id}")
+async def get_id(user_id: int):
+    return f"Вы вошли как пользователь № {user_id}"
+
+
+@app.get("/users/{username}/{age}")
+async def inf(username: str, age: int):
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
